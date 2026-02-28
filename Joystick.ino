272f05a8c33727863266ae6372d4d2291179f9ca
miki1106 <108895954+mihael1106@users.noreply.github.com>
2026-02-28 12:23:02 +0100

    Added buttons
    created const.h file containing constants

--- Joystick.ino
+++ Joystick.ino
@@ -1,3 +1,5 @@
+#include "const.h"
+
 typedef struct {
   int16_t buttons;
   int16_t joy_x;
@@ -11,18 +13,31 @@ joystick_packet_t joystick = {0};
 
 void setup() {
   Serial.begin(115200);
-  pinMode(11, INPUT);
-  pinMode(12, INPUT);
-  pinMode(10, INPUT_PULLUP);
+  pinMode(JOYX, INPUT);
+  pinMode(JOYY, INPUT);
+  pinMode(JOYBTN, INPUT_PULLUP);
+
+  pinMode(CUCANJ_SAG, INPUT_PULLUP);
+  pinMode(SHIELD, INPUT_PULLUP);
+  pinMode(PUCANJE, INPUT_PULLUP);
+  pinMode(SKOK, INPUT_PULLUP);
 }
 
 void loop() {
   delay(100);
+  int16_t buttons = 0;
+  buttons |= (!digitalRead(CUCANJ_SAG) ? 1 : 0) << 0;
+  buttons |= (!digitalRead(SHIELD) ? 1 : 0) << 1;
+  buttons |= (!digitalRead(PUCANJE) ? 1 : 0) << 2;
+  buttons |= (!digitalRead(SKOK) ? 1 : 0) << 3;
+  buttons |= (!digitalRead(JOYBTN) ? 1 : 0) << 4;
   int x = (int)((analogRead(11)-2048) * 0.0488519785051294577430385930);
   int y = (int)((analogRead(12)-2048) * 0.0488519785051294577430385930);
   joystick.joy_x = x;
   joystick.joy_y = y;
+  joystick.buttons = buttons;
   Serial.write((uint8_t*)&joystick, sizeof(joystick));
+  //Serial.println(buttons);
   /*Serial.print(x);
   Serial.print(" ");
   Serial.print(y);
---
+++ const.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CUCANJ_SAG 41
+#define SHIELD 39
+#define PUCANJE 40
+#define SKOK 14
+
+#define JOYBTN 10
+#define JOYX 11
+#define JOYY 12
