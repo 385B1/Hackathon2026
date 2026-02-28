7acd9c8b21dbca57f0598c7ecb2c47dd817635d3
miki1106 <108895954+mihael1106@users.noreply.github.com>
2026-02-28 16:20:16 +0100

    Sleep now works and led
    led library is kinda broken so manual repair may be needed

--- Joystick.ino
+++ Joystick.ino
@@ -1,5 +1,8 @@
 #include "const.h"
+#include <Wire.h>
 #include <Adafruit_LSM6DSOX.h>
+#include <FastLED.h>
+#include "ColorConverterLib.h"
 
 typedef struct {
   int16_t buttons;
@@ -10,18 +13,30 @@ typedef struct {
   int16_t tilt_z;
 } joystick_packet_t;
 
-joystick_packet_t joystick = {0};
+int32_t millis_var;
+bool sleep_ = false;
+
+joystick_packet_t current = {0};
 
 Adafruit_LSM6DSOX sox;
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(115200);
   
+  FastLED.addLeds<WS2812, 2>(leds, NUM_LEDS); 
+  
   Wire.begin(SDA, SCL);
 
   if (!sox.begin_I2C(I2CPROTOCOL)) {
+    leds[0] = 0x00FF00;
+    FastLED.show();
     while (1);
   }
+  leds[0] = 0xFF0000;
+  FastLED.show();
+  delay(2000);
 
   pinMode(JOYX, INPUT);
   pinMode(JOYY, INPUT);
@@ -33,29 +48,57 @@ void setup() {
   pinMode(SKOK, INPUT_PULLUP);
 }
 
+
+
 void loop() {
   delay(50);
-  int16_t buttons = 0;
-  buttons |= (!digitalRead(CUCANJ_SAG) ? 1 : 0) << 0;
-  buttons |= (!digitalRead(SHIELD) ? 1 : 0) << 1;
-  buttons |= (!digitalRead(PUCANJE) ? 1 : 0) << 2;
-  buttons |= (!digitalRead(SKOK) ? 1 : 0) << 3;
-  buttons |= (!digitalRead(JOYBTN) ? 1 : 0) << 4;
-  joystick.buttons = buttons;
   
-  int x = (int)((analogRead(JOYX)-2048) * 0.0488519785051294577430385930);
-  int y = (int)((analogRead(JOYY)-2048) * 0.0488519785051294577430385930);
-  joystick.joy_x = x;
-  joystick.joy_y = y;
+  joystick_packet_t previous; 
+
+  memcpy(&previous, &current, sizeof(current));
   
-  sensors_event_t accel;
-  sensors_event_t gyro;
-  sensors_event_t temp;
-  sox.getEvent(&accel, &gyro, &temp);
+  if (!sleep_) {
+    int16_t buttons = 0;
+    buttons |= (!digitalRead(CUCANJ_SAG) ? 1 : 0) << 0;
+    buttons |= (!digitalRead(SHIELD) ? 1 : 0) << 1;
+    buttons |= (!digitalRead(PUCANJE) ? 1 : 0) << 2;
+    buttons |= (!digitalRead(SKOK) ? 1 : 0) << 3;
+    buttons |= (!digitalRead(JOYBTN) ? 1 : 0) << 4;
+    current.buttons = buttons;
 
-  joystick.tilt_x = map(accel.acceleration.x, -5, 5, MIN_RANGE, MAX_RANGE);
-  joystick.tilt_y = map(accel.acceleration.y, -5, 5, MIN_RANGE, MAX_RANGE);
-  joystick.tilt_z = map(accel.acceleration.z, -5, 5, MIN_RANGE, MAX_RANGE);
+    int x = (int)((analogRead(JOYX)-2048) * 0.0488519785051294577430385930);
+    int y = (int)((analogRead(JOYY)-2048) * 0.0488519785051294577430385930);
+    current.joy_x = x;
+    current.joy_y = y;
 
-  Serial.write((uint8_t*)&joystick, sizeof(joystick));
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    sox.getEvent(&accel, &gyro, &temp);
+
+    current.tilt_x = map(accel.acceleration.x, -5, 5, MIN_RANGE, MAX_RANGE);
+    current.tilt_y = map(accel.acceleration.y, -5, 5, MIN_RANGE, MAX_RANGE);
+    current.tilt_z = map(accel.acceleration.z, -5, 5, MIN_RANGE, MAX_RANGE);
+
+    Serial.write((uint8_t*)&current, sizeof(current));
+  }
+
+  uint8_t r, g, b;
+  ColorConverter::HsvToRgb(millis()/3600.0, 1.0, 1.0, r, g, b);
+  leds[0] = (r << 16) | (g << 8) | b;
+  FastLED.show();
+  
+  if(memcmp(&current, &previous, sizeof(current)) != 0) {
+    millis_var = millis();
+    sleep_ = false;
+  }
+  
+  if (millis() - millis_var > 29297) {
+    sleep_ = true;
+  }
+
+  if(digitalRead(CUCANJ_SAG) == 0) {
+    sleep_ = false;
+    millis_var = millis();
+  }
 }
--- const.h
+++ const.h
@@ -1,18 +1,20 @@
 #pragma once
-// mapping
-#define MIN_RANGE -100
-#define MAX_RANGE 100
+
+//led
+#define NUM_LEDS 1
 
 // accelerometer
 #define SDA 8
 #define SCL 9
 #define I2CPROTOCOL 0x6B
+#define MIN_RANGE -150
+#define MAX_RANGE 150
 
 // buttons
-#define CUCANJ_SAG 41
-#define SHIELD 39
-#define PUCANJE 40
-#define SKOK 14
+#define CUCANJ_SAG 41 // btn 1
+#define SHIELD 39 // btn 2
+#define PUCANJE 40 // btn 3
+#define SKOK 14 // btn 4
 
 // joystick
 #define JOYBTN 10
