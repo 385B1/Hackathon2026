47257c77b7ce0935fd2f8bf05e89a7ebdf002f3b
miki1106 <108895954+mihael1106@users.noreply.github.com>
2026-02-28 14:37:10 +0100

    added accelerometer
    type shii

--- Joystick.ino
+++ Joystick.ino
@@ -1,4 +1,5 @@
 #include "const.h"
+#include <Adafruit_LSM6DSOX.h>
 
 typedef struct {
   int16_t buttons;
@@ -11,8 +12,17 @@ typedef struct {
 
 joystick_packet_t joystick = {0};
 
+Adafruit_LSM6DSOX sox;
+
 void setup() {
   Serial.begin(115200);
+  
+  Wire.begin(SDA, SCL);
+
+  if (!sox.begin_I2C(I2CPROTOCOL)) {
+    while (1);
+  }
+
   pinMode(JOYX, INPUT);
   pinMode(JOYY, INPUT);
   pinMode(JOYBTN, INPUT_PULLUP);
@@ -24,23 +34,28 @@ void setup() {
 }
 
 void loop() {
-  delay(100);
+  delay(50);
   int16_t buttons = 0;
   buttons |= (!digitalRead(CUCANJ_SAG) ? 1 : 0) << 0;
   buttons |= (!digitalRead(SHIELD) ? 1 : 0) << 1;
   buttons |= (!digitalRead(PUCANJE) ? 1 : 0) << 2;
   buttons |= (!digitalRead(SKOK) ? 1 : 0) << 3;
   buttons |= (!digitalRead(JOYBTN) ? 1 : 0) << 4;
-  int x = (int)((analogRead(11)-2048) * 0.0488519785051294577430385930);
-  int y = (int)((analogRead(12)-2048) * 0.0488519785051294577430385930);
+  joystick.buttons = buttons;
+  
+  int x = (int)((analogRead(JOYX)-2048) * 0.0488519785051294577430385930);
+  int y = (int)((analogRead(JOYY)-2048) * 0.0488519785051294577430385930);
   joystick.joy_x = x;
   joystick.joy_y = y;
-  joystick.buttons = buttons;
+  
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  sox.getEvent(&accel, &gyro, &temp);
+
+  joystick.tilt_x = map(accel.acceleration.x, -5, 5, MIN_RANGE, MAX_RANGE);
+  joystick.tilt_y = map(accel.acceleration.y, -5, 5, MIN_RANGE, MAX_RANGE);
+  joystick.tilt_z = map(accel.acceleration.z, -5, 5, MIN_RANGE, MAX_RANGE);
+
   Serial.write((uint8_t*)&joystick, sizeof(joystick));
-  //Serial.println(buttons);
-  /*Serial.print(x);
-  Serial.print(" ");
-  Serial.print(y);
-  Serial.print(" ");
-  Serial.println(digitalRead(10 ) == LOW);*/
 }
--- const.h
+++ const.h
@@ -1,10 +1,20 @@
 #pragma once
+// mapping
+#define MIN_RANGE -100
+#define MAX_RANGE 100
 
+// accelerometer
+#define SDA 8
+#define SCL 9
+#define I2CPROTOCOL 0x6B
+
+// buttons
 #define CUCANJ_SAG 41
 #define SHIELD 39
 #define PUCANJE 40
 #define SKOK 14
 
+// joystick
 #define JOYBTN 10
 #define JOYX 11
 #define JOYY 12
